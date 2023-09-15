f48c9aaf7349763f154b6d05c83694d07098be1a
Beatriz <69567720+mateuscoripiom@users.noreply.github.com>
2023-09-15 11:30:33 -0300

    15.09

--- AppBanco/Program.cs
+++ AppBanco/Program.cs
@@ -3,16 +3,17 @@ using System;
 
 namespace AppBanco
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Banco db = new Banco();
+           
             UsuarioDAO ObjDao = new UsuarioDAO();
-            db.Open();
 
-            MySqlConnection Conexao = new MySqlConnection(@"Server = Localhost; database = dbAppBanco; user = root; password = 12345678");
-            Conexao.Open();
+            Console.WriteLine("Informe o ID para identificação: ");
+            string IdUsu = Console.ReadLine();
+            string strDado = ObjDao.SelectDado(IdUsu);
+            Console.WriteLine("Olá senhor(a) " + strDado);
 
             Console.WriteLine("Digite o nome: ");
             string strNomeUsu = Console.ReadLine();
@@ -20,48 +21,15 @@ namespace AppBanco
             Console.WriteLine("Digite o cargo: ");
             string strCargo = Console.ReadLine();
 
-            Console.WriteLine("Digite o Id: ");
-            string strIdUsu = Console.ReadLine();
-
             Console.WriteLine("Informe a data de nascimento: ");
             string strDataNasc = Console.ReadLine();
 
-            db.Open();
-            ObjDao.Insert(strNomeUsu, strCargo, strDataNasc);
             
-            string strUpdate = "update tbUsuario set NomeUsu = 'João' where IdUsu = 4;";
-            db.ExecuteNowdSql(strUpdate);
+            ObjDao.Insert(strNomeUsu, strCargo, strDataNasc);
 
-            string strDelete = "delete from tbUsuario where IdUsu = 3;";
-            db.ExecuteNowdSql(strDelete);
-            db.Close();
+            ObjDao.Select();
 
-            db.Open();
-            string strSelect = "select * from tbUsuario;";
- 
-
-
-            //ObjCommand.CommandText = "select * from tbUsuario;";
-            //ObjCommand.CommandType = System.Data.CommandType.Text;
-            //ObjCommand.Connection = Conexao;
-
-            MySqlDataReader leitor = db.ExecuteReadSql(strSelect);
-            while (leitor.Read())
-            {
-                Console.WriteLine("Código = {0} | Nome= {1} | Cargo = {2} | Nascimento = {3}",
-                    leitor["IdUsu"], leitor["NomeUsu"], leitor["Cargo"],leitor["DataNasc"]);
-            }
-            leitor.Close();
-
-            Console.WriteLine("Informe o ID para identificação: ");
-            string IdUsu = Console.ReadLine();
-            string strSelectId = "select NomeUsu from tbUsuario where IdUsu =" + IdUsu + ";";
-            string strDado = db.ExecuteScalarSql(strSelectId);
-            Console.WriteLine("Olá senhor(a) " + strDado);
-
-
-            db.Open();
-            Console.ReadLine(); 
+            Console.ReadLine();
 
         }
     }
--- AppBanco/UsuarioDAO.cs
+++ AppBanco/UsuarioDAO.cs
@@ -1,10 +1,11 @@
-﻿namespace AppBanco
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace AppBanco
 {
-    internal class UsuarioDAO
+    class UsuarioDAO
     {
         Banco db = new Banco();
-
-
         public void Insert(string strNomeUsu, string strCargo, string strDataNasc)
         {
             string strInsert = string.Format("insert into tbUsuario(NomeUsu, Cargo, DataNasc)" +
@@ -14,11 +15,13 @@
             db.ExecuteNowdSql(strInsert);
             db.Close();
         }
+      
+
 
         public void Update(string strNomeUsu, string strCargo, string strDataNasc, string strIdUsu)
         {
-
             db.Open();
+
             string strUpdate = string.Format("update into tbUsuario(NomeUsu, Cargo, DataNasc)" +
                                              "values('{0}', '{1}', STR_TO_DATE('{2}', '%d/%m/%Y %H:%i:%s')" +
                                              "where IdUsu = {3};", strNomeUsu, strCargo, strDataNasc, strIdUsu);
@@ -38,13 +41,31 @@
 
         public string SelectDado(string strIdUsu)
         {
-            string strDado = "select NomeUsu from tbUsuario where IdUsu = " + strIdUsu + ";");
+            string strDado = ("select NomeUsu from tbUsuario where IdUsu = " + strIdUsu + ";");
 
             db.Open();
             strDado = db.ExecuteScalarSql(strDado);
             db.Close();
 
             return strDado;
+        }
+
+        public MySqlDataReader Select()
+        {
+
+            string strSelect = "select * from tbUsuario;";
+            db.Open();
+
+            MySqlDataReader leitor = db.ExecuteReadSql(strSelect);
+            while (leitor.Read())
+            {
+                Console.WriteLine("Código = {0} | Nome= {1} | Cargo = {2} | Nascimento = {3}",
+                    leitor["IdUsu"], leitor["NomeUsu"], leitor["Cargo"], leitor["DataNasc"]);
+            }
+            leitor.Close();
+            db.Close();
+
+            return leitor;
         }
     }
 }
